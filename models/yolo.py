4195228f4fc4c28a542ae07abcf268c68454a032
Aquino Nyapara <aquino.nyapara@gmail.com>
2024-08-20 12:15:54 +0300

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -797,6 +797,14 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
+        elif m in [C2f]:
+            c1,c2 = ch[f], args[0]
+            if c2!= no:
+                c2 = make_divisible(c2 * gw, 8)
+            args = [c1, c2]
+            if m in [C2f]:
+                args.inser(2, n) #number of repeats
+                n = 1
         else:
             c2 = ch[f]
 
